73d95c1f7848ec8da7f0baf02059d7f438f7ca31
MohamedHAMMA <mohamedhamma445@gmail.com>
2023-12-12 23:54:47 +0100

    suite and others examples

--- first-serveurs/express.js
+++ first-serveurs/express.js
@@ -1,18 +1,17 @@
-var express =require('express');
+var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.get('/',function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('../public/express01.html', function (err,data) {
-        res.end(data);
-    });
+app.set('view engine', 'jade');
+app.set('views', __dirname);
+
+app.get('/', function (req, res) {
+  res.render('express02', { title: 'Hello', names: [ 'Pierre', 'Paul', 'Jacques' ] });
 });
 
-app.get('/hello/:name', function(req,res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: 'Hello ' + req.params.name + '!'}));
+app.get('/hello/:name', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Hello ' + req.params.name + '!' }));
 });
 
-app.listen(8080)
-console.log('Server running at http://127.0.0.1:8080/');
+app.listen(8080);
--- first-serveurs/hello.js
+++ first-serveurs/hello.js
@@ -29,13 +29,11 @@ var server = http.createServer(function (req, res) {
     var url_parts = url.parse(req.url, true);
     var name = url_parts.query.name;
     if (name) {
-        console.log('Name: ' +name);
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({message: 'Hello ' +name + '!'}));
     } else {
-        console.log('No name!');
         res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('../public/hello02.html',function (err,data) {
+        fs.readFile('../public/hello02.html', function (err,data) {
             res.end(data);
         });
     }
